b92cb47d999c3b5dd2716ed6e2fafe005e9f4e0e
sunfkny <30853461+sunfkny@users.noreply.github.com>
2023-03-29 10:09:26 +0800

    make_password, check_password

--- backend/apps/user/models.py
+++ backend/apps/user/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
 
 from backend.settings import DB_PREFIX, DEFAULT_AVATAR
 
@@ -10,6 +11,15 @@ class User(models.Model):
     username = models.CharField(max_length=64, verbose_name="用户名")
     password = models.CharField(null=True, blank=True, max_length=255, verbose_name="密码")
     avatar = models.CharField(default=DEFAULT_AVATAR, max_length=255, verbose_name="头像")
+
+    def make_password(self, password):
+        self.password = make_password(password)
+        self.save(update_fields=["password"])
+
+    def check_password(self, password) -> bool:
+        if self.password is None:
+            return False
+        return check_password(password=password, encoded=self.password)
 
     class Meta:
         db_table = f"{DB_PREFIX}_user"
