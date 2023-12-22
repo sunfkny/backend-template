dcc361f75ce767e36a6c0450f2fed18d0e4346ef
sunfkny <30853461+sunfkny@users.noreply.github.com>
2023-12-22 15:18:05 +0800

    better exception_handler

--- backend/urls.py
+++ backend/urls.py
@@ -21,12 +21,14 @@ from typing import Type, TypeAlias, Union
 from django.contrib import admin
 from django.core.paginator import InvalidPage
 from django.db.models.fields.files import FieldFile
+from django.db.utils import DatabaseError
 from django.http import HttpRequest, HttpResponse
 from django.urls import include, path
 from ninja import NinjaAPI
 from ninja.errors import AuthenticationError, ValidationError
 from ninja.renderers import JSONRenderer
 from ninja.responses import NinjaJSONEncoder
+from requests import RequestException
 
 from backend.settings import USE_TZ
 
@@ -63,9 +65,12 @@ api_back = NinjaAPI(
 def set_exception_handlers(api: NinjaAPI):
     @api.exception_handler(AuthenticationError)
     def authentication_error_handler(request: HttpRequest, exc: AuthenticationError) -> HttpResponse:
+        error_str = str(exc)
+        if error_str:
+            logger.warning(error_str)
         return api.create_response(
             request,
-            {"code": 401, "msg": "登录失效"},
+            {"code": 401, "msg": error_str or "登录失效"},
             status=401,
         )
 
@@ -73,7 +78,10 @@ def set_exception_handlers(api: NinjaAPI):
     def validation_error_handler(request: HttpRequest, exc: ValidationError) -> HttpResponse:
         logger.warning(exc.errors)
         if request.method == "POST":
-            logger.warning(request.body)
+            try:
+                logger.warning(request.body)
+            except Exception:
+                pass
         return api.create_response(
             request,
             {"code": 422, "msg": "参数错误", "data": exc.errors},
@@ -88,12 +96,39 @@ def set_exception_handlers(api: NinjaAPI):
             status=400,
         )
 
+    @api.exception_handler(RequestException)
+    def request_exception_handler(request: HttpRequest, exc: RequestException) -> HttpResponse:
+        logger.warning(exc)
+        return api.create_response(
+            request,
+            {"code": -1, "msg": "第三方接口错误"},
+            status=200,
+        )
+
+    @api.exception_handler(DatabaseError)
+    def database_error_handler(request: HttpRequest, exc: DatabaseError) -> HttpResponse:
+        logger.error(exc)
+        return api.create_response(
+            request,
+            {"code": -1, "msg": "数据库错误"},
+            status=200,
+        )
+
+    @api.exception_handler(ValueError)
+    def value_error_handler(request: HttpRequest, exc: ValueError) -> HttpResponse:
+        logger.info(exc)
+        return api.create_response(
+            request,
+            {"code": -1, "msg": f"{exc}"},
+            status=200,
+        )
+
     @api.exception_handler(Exception)
     def exception_handler(request: HttpRequest, exc: Exception) -> HttpResponse:
         logger.exception(exc)
         return api.create_response(
             request,
-            {"code": 500, "msg": f"内部错误: {exc}"},
+            {"code": 500, "msg": "内部错误"},
             status=500,
         )
 
@@ -101,6 +136,9 @@ def set_exception_handlers(api: NinjaAPI):
         authentication_error_handler,
         validation_error_handler,
         invalid_page_handler,
+        request_exception_handler,
+        database_error_handler,
+        value_error_handler,
         exception_handler,
     )
 
