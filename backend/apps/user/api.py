cc90c6e52c32f8900c955e809d2b8f33cd036813
sunfkny <30853461+sunfkny@users.noreply.github.com>
2024-01-16 20:02:23 +0800

    Update api.py

--- backend/apps/user/api.py
+++ backend/apps/user/api.py
@@ -1,10 +1,9 @@
-from django.contrib.auth.hashers import check_password, make_password
-from django.core.paginator import InvalidPage, Paginator
+from django.core.paginator import Paginator
 from django.http import HttpRequest
-from ninja import Form, Header, Query, Router
+from ninja import Form, Header, Query, Body, Router
 
 from backend.settings import REDIS_PREFIX, get_logger
-from backend.utils.auth import auth_admin
+from backend.utils.auth import auth
 from backend.utils.response_types import Response
 
 router = Router(tags=["用户"])
