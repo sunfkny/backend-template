3eff510eee51ca8bbe5a039a9ae26adecd4b8632
sunfkny <30853461+sunfkny@users.noreply.github.com>
2024-03-01 13:25:09 +0800

    shell_plus dont load django models

--- shell_plus.sh
+++ shell_plus.sh
@@ -1,3 +1,10 @@
 source ./venv/bin/activate
 
-python manage.py shell_plus --print-sql --ipython
+python manage.py shell_plus \
+    --print-sql \
+    --ipython \
+    --dont-load auth \
+    --dont-load admin \
+    --dont-load contenttypes \
+    --dont-load sessions
+
