a59d248c5bae72f03ffe05a117a671bcbb4a3e40
sunfkny <30853461+sunfkny@users.noreply.github.com>
2024-04-16 00:14:17 +0800

    Replace DEBUG

--- generate_uwgi.py
+++ generate_uwgi.py
@@ -12,6 +12,7 @@ secret_key = get_random_secret_key()
 source = pathlib.Path("./backend/settings.py").read_text()
 if 'SECRET_KEY = "django-insecure"' in source:
     source = source.replace('SECRET_KEY = "django-insecure"', f'SECRET_KEY = "{secret_key}"')
+    source = source.replace('DEBUG = True', 'DEBUG = False')
     pathlib.Path("./backend/settings.py").write_text(source)
 
 UWSGI_INI_FILE_NAME = f"{DOMAIN_NAME}.ini"
