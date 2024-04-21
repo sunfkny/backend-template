5afda18be46ec6d99928c3ed7b986fcb728e437c
sunfkny <30853461+sunfkny@users.noreply.github.com>
2024-04-21 22:48:54 +0800

    Update format_number.py

--- backend/utils/format_number.py
+++ backend/utils/format_number.py
@@ -1,30 +1,56 @@
 from decimal import Decimal
 
 
-def aggregate_decimal_quantize(value: dict | int | float | Decimal) -> Decimal:
+def aggregate_decimal_quantize(value: dict | int | float | Decimal, digits: int = 2) -> str:
     if isinstance(value, dict):
         value = sum(i for i in value.values() if isinstance(i, (int, float, Decimal)))
     if isinstance(value, (int, float)):
         value = Decimal(value)
-    return value.quantize(Decimal("0.01"))
+    return f"{value:.{digits}f}"
 
 
-def chinese_format_number(num: float | Decimal):
-    if not isinstance(num, (float, Decimal)):
-        return ""
-    if num == 0:
+def intword(
+    value: float | int | Decimal | str | None,
+    digits: int = 1,
+    comma: bool = False,
+    chinese: bool = False,
+) -> str:
+    if not value:
         return "0"
-    if num >= 100000000:
-        formatted_num = "{:.2f}".format(num / 100000000)
-        suffix = "亿"
-    elif num >= 10000:
-        formatted_num = "{:.2f}".format(num / 10000)
-        suffix = "万"
+
+    if chinese:
+        intword_suffix = ((3, "K"), (6, "M"), (9, "B"), (12, "T"))
+        max_exponent = intword_suffix[0][0]
+        min_exponent = intword_suffix[-1][0]
+        max_suffix = intword_suffix[-1][1]
     else:
-        formatted_num = "{:.2f}".format(num)
-        suffix = ""
-    if formatted_num.endswith(".00"):
-        formatted_num = formatted_num[:-3]
-    elif formatted_num.endswith("0"):
-        formatted_num = formatted_num[:-1]
-    return formatted_num + suffix
+        intword_suffix = ((4, "万"), (8, "亿"), (12, "兆"), (16, "京"))
+        max_exponent = intword_suffix[0][0]
+        min_exponent = intword_suffix[-1][0]
+        max_suffix = intword_suffix[-1][1]
+
+    if comma:
+        format_str = f",.{digits}f"
+    else:
+        format_str = f".{digits}f"
+
+    try:
+        value = float(value)
+    except (TypeError, ValueError):
+        return str(value)
+
+    abs_value = abs(value)
+
+    if abs_value < 10**min_exponent:
+        return f"{value:{format_str}}"
+
+    if abs_value > 10**max_exponent:
+        value = value / 10**max_exponent
+        return f"{value:{format_str}}{max_suffix}"
+
+    for exponent, suffix in reversed(intword_suffix):
+        if abs_value >= 10**exponent:
+            value = value / 10**exponent
+            return f"{value:{format_str}}{suffix}"
+
+    return str(value)
