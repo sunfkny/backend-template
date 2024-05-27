15fa2e9b2f01479b7448b90f7e80fdaff4ab0a65
sunfkny <30853461+sunfkny@users.noreply.github.com>
2024-05-27 12:33:34 +0800

    Update snowflake.py

--- backend/utils/snowflake.py
+++ backend/utils/snowflake.py
@@ -1,6 +1,14 @@
 import os
 import time
 import uuid
+from typing import NamedTuple
+
+
+class SnowflakeData(NamedTuple):
+    timestamp: int
+    datacenter_id: int
+    worker_id: int
+    sequence: int
 
 
 class InvalidSystemClock(Exception):
@@ -20,23 +28,21 @@ class Snowflake:
     # 最大取值计算
     MAX_WORKER_ID = (1 << WORKER_ID_BITS) - 1
     MAX_DATACENTER_ID = (1 << DATACENTER_ID_BITS) - 1
+    MAX_SEQUENCE = (1 << SEQUENCE_BITS) - 1
 
     # 移位偏移计算
     WOKER_ID_SHIFT = SEQUENCE_BITS
     DATACENTER_ID_SHIFT = SEQUENCE_BITS + WORKER_ID_BITS
-    TIMESTAMP_LEFT_SHIFT = SEQUENCE_BITS + WORKER_ID_BITS + DATACENTER_ID_BITS
-
-    # 序号循环掩码
-    SEQUENCE_MASK = (1 << SEQUENCE_BITS) - 1
+    TIMESTAMP_SHIFT = SEQUENCE_BITS + WORKER_ID_BITS + DATACENTER_ID_BITS
 
     # Twitter元年时间戳
     TWEPOCH = 1288834974657
 
     def __init__(self, datacenter_id: int | None = None, worker_id: int | None = None):
         if worker_id is None:
-            worker_id = os.getpid() % (2**self.WORKER_ID_BITS)
+            worker_id = os.getpid() & self.MAX_WORKER_ID
         if datacenter_id is None:
-            datacenter_id = uuid.getnode() % (2**self.DATACENTER_ID_BITS)
+            datacenter_id = uuid.getnode() & self.MAX_DATACENTER_ID
 
         if worker_id > self.MAX_WORKER_ID or worker_id < 0:
             raise ValueError(f"Worker ID can't be greater than {self.MAX_WORKER_ID} or less than 0")
@@ -62,7 +68,7 @@ class Snowflake:
             raise InvalidSystemClock(f"Clock is moving backwards, rejecting requests until {self.last_timestamp}")
 
         if timestamp == self.last_timestamp:
-            self.sequence = (self.sequence + 1) & self.SEQUENCE_MASK
+            self.sequence = (self.sequence + 1) & self.MAX_SEQUENCE
             if self.sequence == 0:
                 timestamp = self.till_next_millis(self.last_timestamp)
         else:
@@ -71,12 +77,25 @@ class Snowflake:
         self.last_timestamp = timestamp
 
         new_id = (
-            ((timestamp - self.TWEPOCH) << self.TIMESTAMP_LEFT_SHIFT)
+            ((timestamp - self.TWEPOCH) << self.TIMESTAMP_SHIFT)
             | (self.datacenter_id << self.DATACENTER_ID_SHIFT)
             | (self.worker_id << self.WOKER_ID_SHIFT)
             | self.sequence
         )
         return new_id
+
+    @classmethod
+    def parse(cls, snowflake: int):
+        timestamp = (snowflake >> cls.TIMESTAMP_SHIFT) + cls.TWEPOCH
+        datacenter_id = snowflake >> cls.DATACENTER_ID_SHIFT & cls.MAX_WORKER_ID
+        worker_id = snowflake >> cls.WOKER_ID_SHIFT & cls.MAX_DATACENTER_ID
+        sequence = snowflake & cls.MAX_SEQUENCE
+        return SnowflakeData(
+            timestamp=timestamp,
+            datacenter_id=datacenter_id,
+            worker_id=worker_id,
+            sequence=sequence,
+        )
 
     def till_next_millis(self, last_timestamp: int):
         """
