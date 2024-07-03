fe288f280d4fe815ce16b08d852c6f55836736b1
sunfkny <30853461+sunfkny@users.noreply.github.com>
2024-07-03 10:36:40 +0800

    Update typed_model.py

--- backend/utils/typed_model.py
+++ backend/utils/typed_model.py
@@ -1,10 +1,14 @@
-from typing import TYPE_CHECKING
+from __future__ import annotations
+
+from collections.abc import Iterable, Sequence
+from typing import TYPE_CHECKING, Any, ClassVar, Generic, Literal
+
+from django.db import models
+from django.db.models import BaseConstraint, Index
+from django.db.models.query import QuerySet
+from typing_extensions import TypeVar
 
 if TYPE_CHECKING:
-    from collections.abc import Sequence
-    from typing import Any, ClassVar, Literal
-
-    from django.db.models import BaseConstraint, Index
 
     class TypedModelMeta:
         """
@@ -38,6 +42,25 @@ if TYPE_CHECKING:
         constraints: ClassVar[list[BaseConstraint]]
         verbose_name: ClassVar[str | Any]
         verbose_name_plural: ClassVar[str | Any]
-
 else:
     TypedModelMeta = object
+
+
+_To = TypeVar("_To", bound=models.Model)
+
+if TYPE_CHECKING:
+    from typing import type_check_only
+
+    @type_check_only
+    class RelatedManager(models.Manager[_To], Generic[_To]):
+        related_val: tuple[int, ...]
+
+        def add(self, *objs: _To | int, bulk: bool = ...) -> None: ...
+        async def aadd(self, *objs: _To | int, bulk: bool = ...) -> None: ...
+        def remove(self, *objs: _To | int, bulk: bool = ...) -> None: ...
+        async def aremove(self, *objs: _To | int, bulk: bool = ...) -> None: ...
+        def set(self, objs: QuerySet[_To] | Iterable[_To | int], *, bulk: bool = ..., clear: bool = ...) -> None: ...
+        async def aset(self, objs: QuerySet[_To] | Iterable[_To | int], *, bulk: bool = ..., clear: bool = ...) -> None: ...
+        def clear(self) -> None: ...
+        async def aclear(self) -> None: ...
+        def __call__(self, *, manager: str) -> RelatedManager[_To]: ...
