c2bdb9ca470e9aa8ea402ce1e8c8b9f13fc8cf13
sunfkny <30853461+sunfkny@users.noreply.github.com>
2024-08-13 09:46:58 +0800

    Update shell_plus.sh

--- shell_plus.sh
+++ shell_plus.sh
@@ -1,10 +1,22 @@
-source ./venv/bin/activate
+set -e
 
-python manage.py shell_plus \
+cd `dirname $0`
+
+if [ -f .venv/bin/python ]; then
+    PYTHON=.venv/bin/python
+elif [ -f venv/bin/python ]; then
+    PYTHON=venv/bin/python
+else
+    echo "Virtual environment not found."
+    exit 1
+fi
+
+echo "Using Python: $PYTHON"
+
+$PYTHON manage.py shell_plus \
     --print-sql \
     --ipython \
     --dont-load auth \
     --dont-load admin \
     --dont-load contenttypes \
     --dont-load sessions
-
