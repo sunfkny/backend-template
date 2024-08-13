6c7c77397462b8b57fce8dea66c1d7c8da9976f6
sunfkny <30853461+sunfkny@users.noreply.github.com>
2024-08-13 14:50:32 +0800

    Update ninja-stubs

--- typings/ninja-stubs/params/__init__.pyi
+++ typings/ninja-stubs/params/__init__.pyi
@@ -1,1 +1,8 @@
-from ninja.params.functions import *
+from ninja.params.functions import Body as Body
+from ninja.params.functions import Cookie as Cookie
+from ninja.params.functions import File as File
+from ninja.params.functions import Form as Form
+from ninja.params.functions import Header as Header
+from ninja.params.functions import Path as Path
+from ninja.params.functions import Query as Query
+
