9f592a1a5bce4b56a47076df7776edd036bac2fd
sunfkny <30853461+sunfkny@users.noreply.github.com>
2024-09-15 18:25:48 +0800

    Remove debug dist static

--- backend/urls.py
+++ backend/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path
 from ninja import NinjaAPI, Swagger
 
-from backend.settings import DEBUG, DIST_ROOT, MEDIA_ROOT, MEDIA_URL_RELATIVE_PATH
+from backend.settings import DEBUG, MEDIA_ROOT, MEDIA_URL_RELATIVE_PATH
 
 from .handler import set_exception_handlers
 from .renderer import CustomJSONRenderer
@@ -63,5 +63,4 @@ urlpatterns = [
 
 
 if DEBUG:
-    urlpatterns += static(MEDIA_URL_RELATIVE_PATH, document_root=MEDIA_ROOT, show_indexes=True)
-    urlpatterns += static("/", document_root=DIST_ROOT, show_indexes=True)
+    urlpatterns += static(MEDIA_URL_RELATIVE_PATH, document_root=MEDIA_ROOT)
