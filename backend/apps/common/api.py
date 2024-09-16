3b2560382c33e998d91b3ce2f522cf36ee7efc72
sunfkny <30853461+sunfkny@users.noreply.github.com>
2024-09-17 01:17:46 +0800

    Add get_system_info

--- backend/apps/common/api.py
+++ backend/apps/common/api.py
@@ -1,3 +1,10 @@
+import os
+import platform
+import sys
+from typing import Any
+
+import psutil
+from django.contrib.admin.views.decorators import staff_member_required
 from django.core.paginator import Paginator
 from django.http import HttpRequest
 from ninja import Body, Form, Header, Query, Router
@@ -78,3 +85,87 @@ def get_status(
             "migrations": migrations_status,
         }
     )
+
+
+@router.get("system/info")
+@staff_member_required
+def get_system_info(request: HttpRequest):
+    os_data = {
+        "name": os.name,
+        "getcwd": os.getcwd(),
+        "getpid": os.getpid(),
+        "cpu_count": os.cpu_count(),
+        "getloadavg": os.getloadavg(),
+    }
+
+    sys_data = {
+        "version_info": {
+            "major": sys.version_info.major,
+            "minor": sys.version_info.minor,
+            "micro": sys.version_info.micro,
+            "releaselevel": sys.version_info.releaselevel,
+            "serial": sys.version_info.serial,
+        },
+        "platform": sys.platform,
+        "version": sys.version,
+    }
+
+    platform_data: dict[str, Any] = {
+        "machine": platform.machine(),
+        "node": platform.node(),
+        "system": platform.system(),
+        "release": platform.release(),
+        "version": platform.version(),
+        "platform": platform.platform(),
+        "python_version": platform.python_version(),
+    }
+    if sys.platform == "win32":
+        platform_data["win32_ver"] = " ".join(platform.win32_ver())
+        platform_data["win32_edition"] = platform.win32_edition()
+    if sys.platform == "linux":
+        platform_data["libc_ver"] = " ".join(platform.libc_ver())
+        try:
+            platform_data["freedesktop_os_release"] = platform.freedesktop_os_release()
+        except Exception:
+            pass
+    if sys.platform == "darwin":
+        platform_data["mac_ver"] = " ".join(platform.mac_ver())
+
+    psutil_data = {
+        "cpu_percent": psutil.cpu_percent(),
+        "cpu_count": psutil.cpu_count(),
+        "cpu_freq": psutil.cpu_freq().current,
+        "swap_memory": psutil.swap_memory()._asdict(),
+        "virtual_memory": psutil.virtual_memory()._asdict(),
+        "net_io_counters": psutil.net_io_counters()._asdict(),
+        "boot_time": psutil.boot_time(),
+        "disk_partitions": [
+            {
+                "disk_partition": disk_partition._asdict(),
+                "disk_usage": psutil.disk_usage(disk_partition.mountpoint)._asdict(),
+            }
+            for disk_partition in psutil.disk_partitions()
+        ],
+    }
+
+    data = {
+        "os": os_data,
+        "sys": sys_data,
+        "platform": platform_data,
+        "psutil": psutil_data,
+    }
+
+    try:
+        import uwsgi  # type: ignore
+
+        data["uwsgi"] = {
+            "masterpid": uwsgi.masterpid(),
+            "version": uwsgi.version.decode(),
+            "hostname": uwsgi.hostname.decode(),
+            "numproc": uwsgi.numproc,
+            "started_on": uwsgi.started_on,
+        }
+    except Exception:
+        pass
+
+    return Response.data(data)
