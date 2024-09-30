e51c579cfc853d9a8d8535c4e7f28a1cbd9404a7
sunfkny <30853461+sunfkny@users.noreply.github.com>
2024-09-30 21:03:05 +0800

    Update tpl

--- backend/apps/user/api.py
+++ backend/apps/user/api.py
@@ -2,6 +2,7 @@ from django.core.paginator import Paginator
 from django.http import HttpRequest
 from ninja import Body, Form, Header, Query, Router
 
+from backend.decorator.response import schema_response
 from backend.response import D, L, Response
 from backend.settings import get_logger
 
