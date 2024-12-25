7fa0da3e98c09dd46350f83a02bd2d10154e62e7
sunfkny <30853461+sunfkny@users.noreply.github.com>
2024-12-25 11:30:42 +0800

    Refactor to IO type for content

--- backend/storage.py
+++ backend/storage.py
@@ -1,19 +1,20 @@
 import hashlib
 import os
+from typing import IO
 
 from django.core.files import File
 from django.core.files.storage import FileSystemStorage
 
 
 class HashedFileSystemStorage(FileSystemStorage):
-    def _get_content_name(self, name: str | None, content: File) -> str:
-        dir_name, file_name = os.path.split(name or content.name)
+    def _get_content_name(self, name: str | None, content: IO) -> str:
+        dir_name, file_name = os.path.split(name or content.name or "")
         file_hash = self._compute_hash(content)
         _, file_ext = os.path.splitext(file_name)
         file_ext = file_ext.lower()
         return os.path.join(dir_name, file_hash + file_ext)
 
-    def _compute_hash(self, content: File) -> str:
+    def _compute_hash(self, content: IO) -> str:
         hasher = hashlib.sha1()
 
         cursor = content.tell()
@@ -25,7 +26,7 @@ class HashedFileSystemStorage(FileSystemStorage):
         finally:
             content.seek(cursor)
 
-    def save(self, name: str | None, content: File, max_length: int | None = None) -> str:
+    def save(self, name: str | None, content: IO, max_length: int | None = None) -> str:
         name = self._get_content_name(name, content)
         if self.exists(name):
             return name
